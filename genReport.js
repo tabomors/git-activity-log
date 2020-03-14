f5db421e84ab9a032b779cd4f39808df9e036e13
Stas Morozevich <stas.morozevich@gmail.com>
2020-03-14 17:11:03 +0300

    Update deps and clean up code a bit

--- genReport.js
+++ genReport.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const shell = require("shelljs");
-const path = require("path");
-const fs = require("fs");
+const shell = require('shelljs');
+const path = require('path');
+const fs = require('fs');
 
-const projects = JSON.parse(fs.readFileSync("./projects.json"));
+const projects = JSON.parse(fs.readFileSync('./projects.json'));
 
-const logsDir = path.join(__dirname, "logs");
+const logsDir = path.join(__dirname, 'logs');
 
 shell.exec(`mkdir -p ${logsDir}`);
 
@@ -14,22 +14,21 @@ const logsToMerge = [];
 
 for (const project of projects) {
   const { authors, pathDir, alias } = project;
-  shell.cd(pathDir).exec("git fetch");
-  shell.cd(__dirname);
 
-  authors.forEach((author, i) => {
-    const logPath = path.join(logsDir, `${alias}${i}.json`);
+  authors.forEach(author => {
+    const logPath = path.join(logsDir, `${alias}_${author}.json`);
     const shellRes = shell
       .exec(`./report.js --path ${pathDir} --author ${author} --all true`)
       .to(logPath);
 
     if (shellRes.code !== 0) {
       shell.echo(shellRes.stderr);
-      shell.exit(1);
+      shell.exit(shellRes.code);
     }
 
     logsToMerge.push(logPath);
   });
 }
 
+// TODO: run it manually
 shell.exec(`./merge.js --logs ${logsToMerge.join(" ")}`);
--- report.js
+++ report.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 const { exec } = require('child_process');
-const crypto = require('crypto');
 const { argv } = require('yargs').boolean('all');
+const nanoid = require('nanoid');
 
-const PARAMS_SEPARATOR = `__ps${crypto.randomBytes(36).toString('hex')}__`;
-const LINES_SEPARATOR = `__ls${crypto.randomBytes(36).toString('hex')}__`;
+const PARAMS_SEPARATOR = nanoid();
+const LINES_SEPARATOR = nanoid();
 
 const now = new Date();
 const currentMonth = `0${now.getMonth() + 1}`.slice(-2);
@@ -18,30 +18,37 @@ const logOptions = `--date=short --reverse ${
 
 const command = formatParams => `cd ${argv.path || '.'}
    git log --pretty=format:"${formatParams.join(
-    PARAMS_SEPARATOR,
-  )}${LINES_SEPARATOR}" ${logOptions}`;
+     PARAMS_SEPARATOR
+   )}${LINES_SEPARATOR}" ${logOptions}`;
 
-const log = (schema, proccessValue = (k, v) => v) => new Promise((resolve, reject) => {
-  const keys = Object.keys(schema);
-  const formatParams = keys.map(key => schema[key]);
+const log = (schema, proccessValue = (k, v) => v) =>
+  new Promise((resolve, reject) => {
+    const keys = Object.keys(schema);
+    const formatParams = keys.map(key => schema[key]);
 
-  exec(command(formatParams), (err, stdout) => {
-    if (err) reject(err);
-    else {
-      const listOfLogs = stdout
-        .split(LINES_SEPARATOR)
-        .filter(line => line.length)
-        .map(line => line.split(PARAMS_SEPARATOR).reduce((obj, value, idx) => {
-          const key = keys[idx];
-          return { ...obj, [key]: proccessValue(key, value) };
-        }, {}));
-      resolve(JSON.stringify(listOfLogs, null, '\t'));
-    }
+    exec(command(formatParams), (err, stdout) => {
+      if (err) reject(err);
+      else {
+        const listOfLogs = stdout
+          .split(LINES_SEPARATOR)
+          .filter(line => line.length)
+          .map(line =>
+            line.split(PARAMS_SEPARATOR).reduce((obj, value, idx) => {
+              const key = keys[idx];
+              return { ...obj, [key]: proccessValue(key, value) };
+            }, {})
+          );
+        resolve(JSON.stringify(listOfLogs, null, '\t'));
+      }
+    });
   });
-});
 
 const SCHEMA = {
-  date: '%ad', message: '%B', refs: '%d', author: '%an',
+  date: '%ad',
+  message: '%B',
+  refs: '%D',
+  authorName: '%an',
+  authorEmail: '%ae'
 };
 
 log(SCHEMA, (k, v) => v.trim())
